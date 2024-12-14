d2ee12eb24e1a043e138b59bd60e34dee49399e2
krishnashray2002 <krishnashraytrivedi447@gmail.com>
2024-12-14 11:46:40 +0530

    changes on branch ba

--- src/practice/Demo.java
+++ src/practice/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("on branch ab");
+		System.out.println("Hello_class1");
+		System.out.println("on branch ba");
 	}
 }
